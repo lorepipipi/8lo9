f4c82d187071ee4edb958dafc616accd09478895
Vina del Mar <vina@duoc.acad>
2024-11-07 09:29:43 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '8lo9',
+  webDir: 'www'
+};
+
+export default config;
