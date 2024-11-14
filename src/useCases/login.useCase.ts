47534226c2988056cb2be8840511038149affce9
lorena <lo.calfin@duocuc.cl>
2024-11-13 22:19:31 -0300

    cambios html

---
+++ src/useCases/login.useCase.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { StorageService } from 'src/managers/StorageService';
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class LoginUseCase {
+  
+    constructor() {}
+
+  }
